48fbe8e3f0b2685b819322196990ce7cdc2b9c4b
asantesylvesterkwame <intelligentson17@gmail.com>
2023-11-30 03:58:23 +0000

    implemented dataPersistence

--- client/App.js
+++ client/App.js
@@ -1,33 +1,70 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Login from './generalPages/Login/Login';
-import Register from './generalPages/Register/Register.js';
-import Welcome from './generalPages/Welcome/Welcome.js';
-
+import React, { useState, useEffect } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./generalScreens/Login/Login";
+import Register from "./generalScreens/Register/Register.js";
+import Welcome from "./generalScreens/Welcome/Welcome.js";
+import Home from "./generalScreens/Home/Home";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import User from "./generalScreens/User/User";
+import TabNavigator from "./components/TabNavigator/TabNavigator";
+import { AuthContext } from "./context/AuthContext.js";
+import { getData } from "./utils/Storage/storage.js";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  useEffect(() => {
+    const checkAuthStatus = async () => {
+      const user = await getData("user");
+      if (user) {
+        setIsSignedIn(true);
+      }
+    };
+
+    checkAuthStatus();
+  }, []);
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Welcome">
-        <Stack.Screen
-          name="Welcome"
-          component={Welcome}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Register"
-          component={Register}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    
+    <AuthContext.Provider
+      value={{ isSignedIn, changeAuthStatus: setIsSignedIn }}
+    >
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Welcome">
+          {isSignedIn ? (
+            <>
+              <Stack.Screen
+                name="Home"
+                component={TabNavigator}
+                options={{ headerShown: false }}
+              />
+            </>
+          ) : (
+            <>
+              <Stack.Screen
+                name="Welcome"
+                component={Welcome}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="Login"
+                component={Login}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="Register"
+                component={Register}
+                options={{ headerShown: false }}
+              />
+            </>
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AuthContext.Provider>
   );
 }

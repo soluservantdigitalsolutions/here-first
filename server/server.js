572804c0155298683977712dce6fdfb4c8f8bec6
asantesylvesterkwame <intelligentson17@gmail.com>
2023-12-05 08:20:43 +0000

    added cors and dotenv

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const dbConnect = require("./dbConnect/dbConnect");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const routes = require("./routes/routes");
 
@@ -14,12 +15,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.use("/", routes);
-dotenv.config()
+app.use(cors());
 
-app.get("/", (req, res)=>{
-    res.send("it is working")
-})
+app.use("/", routes);
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("it is working");
+});
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
   dbConnect();

57ee8b63a2014d6bc4de9824d64fbc9d4d065136
asantesylvesterkwame <intelligentson17@gmail.com>
2023-12-05 08:21:27 +0000

    added auth route

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const authRoutes = require("./auth.route.js");
+const userRoutes = require("./user.route.js");
+
 
 const router = express.Router();
 
 
 const base = "/api/v1"
 router.use(`${base}/auth`, authRoutes);
+router.use(`${base}/users`, userRoutes);
+
 
 
 // Add other route handlers here

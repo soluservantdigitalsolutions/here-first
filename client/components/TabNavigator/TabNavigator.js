b7c037a25cf1bd028908fadf8698a4dbeaad3b8f
asantesylvesterkwame <intelligentson17@gmail.com>
2023-12-05 08:19:35 +0000

    created and configured tab navigator

---
+++ client/components/TabNavigator/TabNavigator.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Home from "../../generalScreens/Home/Home";
+import Icon from "react-native-vector-icons/Ionicons";
+import OrderCart from "../../generalScreens/OrderCart/OrderCart";
+import { COLORS } from "../../constants/color";
+
+const Tab = createBottomTabNavigator();
+
+export default function TabNavigator() {
+  return (
+    <Tab.Navigator tabBarOptions={{ activeTintColor: COLORS.PRIMARY }}>
+      <Tab.Screen
+        name="HomePage"
+        component={Home}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="home-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="OrderCart"
+        component={OrderCart}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="cart-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      {/* <Tab.Screen
+        name="Favorites"
+        component={Favorites}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="heart-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="search-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={Notifications}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="notifications-outline" color={color} size={size} />
+          ),
+        }}
+      /> */}
+    </Tab.Navigator>
+  );
+}

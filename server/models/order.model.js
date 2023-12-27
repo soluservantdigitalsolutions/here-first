2b1cc6fb1ee0be8908017eecd19df0698a66a950
asantesylvesterkwame <intelligentson17@gmail.com>
2023-12-27 09:06:48 +0000

    Edited  Order

--- server/models/order.model.js
+++ server/models/order.model.js
@@ -1,39 +1,45 @@
 const mongoose = require("mongoose");
 
-const OrderSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  food: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Food",
-    required: true,
-  },
-  restaurant: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Restaurant",
-    required: true,
-  },
-  foodPreferences: [
-    {
-      type: String,
+const OrderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-  ],
-  quantity: {
-    type: Number,
-    required: true,
+    food: {
+      id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Food",
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+    },
+    restaurant: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Restaurant",
+      required: true,
+    },
+    foodPreferences: [
+      {
+        type: String,
+      },
+    ],
+    isCompleted: {
+      type: Boolean,
+      default: false,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
   },
-  location: {
-    type: String,
-    required: true,
-  },
-
-  total: {
-    type: Number,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Order", OrderSchema);
